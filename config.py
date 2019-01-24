804bb21eddc4c6890ba8a9f01ee56c5887fa782e
Alex <alexpfonseka@gmail.com>
2019-01-24 17:31:26 +1100

    updated SConscript files

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 def can_build(env, platform):
-    return True
+    return platform == "x11" or platform == "windows" or platform == "osx"
 
 
 def configure(env):
-    pass
+    env.Append(LIBPATH=["#modules/fmod/api/lowlevel/lib/",
+                        "#modules/fmod/api/studio/lib/"])
+    if env["bits"] == "32":
+        env.Append(LIBS=["fmod_vc", "fmodstudio_vc"])
+    else:
+        env.Append(LIBS=["fmod64_vc", "fmodstudio64_vc"])
