a08eb340beedb944cc4e8cb228db6f24132083fa
Pierre-Thomas Meisels <dev001@infra.utopia-rise.com>
2019-04-23 20:08:50 +0200

    TravisCI: try to deploy build to github

--- getFmodAPIScript.sh
+++
@@ -1,3 +0,0 @@
-TOKEN=$(curl -X POST -u $1:$2 https://www.fmod.com/api-login | jq -r '.token')
-URL=$(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" https://www.fmod.com/api-get-download-link\?path\=files/fmodstudio/api/Linux/\&filename\=fmodstudioapi11012linux.tar.gz\&user\=$1 | jq -r '.url')
-wget -O fmodstudioapi11012linux.tar.gz $URL
