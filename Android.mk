b6b083a36660514fcd72b2b47132ba31c07fe146
Pierre-Thomas Meisels <dev001@infra.utopia-rise.com>
2019-05-02 15:35:13 +0200

    CrossPlatformBuild : add ndk-build for android
    This enables to build fmod-gdnative for android.
    This need to be improved by arguments.
    This need to be tested, and industrialized using Travis.

---
+++ Android.mk
@@ -0,0 +1,51 @@
+# Android.mk
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fmod-lowlevel-prebuilt
+LOCAL_SRC_FILES := ../libs/fmod/android/lowlevel/lib/$(TARGET_ARCH_ABI)/libfmod.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := fmod-studio-prebuilt
+LOCAL_SRC_FILES := ../libs/fmod/android/studio/lib/$(TARGET_ARCH_ABI)/libfmodstudio.so
+include $(PREBUILT_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := godot-prebuilt
+ifeq ($(TARGET_ARCH_ABI),x86)
+    LOCAL_SRC_FILES := ../godot-cpp/bin/libgodot-cpp.android.release.i686.a
+endif
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+    LOCAL_SRC_FILES := ../godot-cpp/bin/libgodot-cpp.android.release.armv7-a.a
+endif
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+    LOCAL_SRC_FILES := ../godot-cpp/bin/libgodot-cpp.android.release.armv8-a.a
+endif
+include $(PREBUILT_STATIC_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := android_fmod_gdnative
+LOCAL_CPPFLAGS := -std=c++14
+LOCAL_CPP_FEATURES := rtti exceptions
+LOCAL_LDLIBS := -llog
+
+LOCAL_SRC_FILES := \
+godot_fmod.cpp \
+gdlibrary.cpp \
+
+LOCAL_SHARED_LIBRARIES := \
+fmod-lowlevel-prebuilt \
+fmod-studio-prebuilt
+
+LOCAL_C_INCLUDES := \
+../godot-cpp/godot_headers \
+../godot-cpp/include/ \
+../godot-cpp/include/core \
+../godot-cpp/include/gen \
+../libs/fmod/android/studio/inc \
+../libs/fmod/android/lowlevel/inc
+
+LOCAL_STATIC_LIBRARIES := godot-prebuilt
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ jni/Application.mk
@@ -0,0 +1,3 @@
+# Application.mk
+APP_STL:=c++_shared
+APP_ABI:=arm64-v8a x86 armeabi-v7a
