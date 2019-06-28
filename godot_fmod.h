98de48dae7f74c5075409465d7d4e7affacd2699
piiertho <48322356+piiertho@users.noreply.github.com>
2019-06-28 14:46:47 +0200

    Performance data (#23)
    * PerformanceData : Helper method to query performance data.
    
    This enable to get cpu and mem infos of Fmod.
    Conforms our repo to module version.
    
    * PerformanceData : Optimize performancedata by creating instances of dictionaries at construct time

--- godot_fmod.h
+++ godot_fmod.h
@@ -42,6 +42,9 @@ namespace godot {
         // referenced through uuids generated in script
         std::map<const uint64_t, FMOD::Studio::EventInstance *> unmanagedEvents;
 
+        //Store disctionnary of performance data
+        Dictionary performanceData;
+
     GODOT_CLASS(GodotFmod, Node)
 
     private:
@@ -127,6 +130,7 @@ namespace godot {
         Array getAvailableDrivers();
         int getDriver();
         void setDriver(int id);
+        Dictionary getPerformanceData();
         void setGlobalParameter(String parameterName, float value);
         float getGlobalParameter(String parameterName);
 
