66fdbf8eb09550a96348722e7869f02560c02154
Pierre-Thomas Meisels <dev001@infra.utopia-rise.com>
2019-08-07 16:04:59 +0200

    GodotUnitTesting : modify run_test script to run it on travis

--- demo/run_tests.sh
+++ demo/run_tests.sh
@@ -1,2 +1,15 @@
-alias godot=/Applications/Godot.app/Contents/MacOS/Godot
-godot -d -s --path $PWD addons/gut/gut_cmdln.gd
+alias godot=$1
+godot -d -s --path $PWD addons/gut/gut_cmdln.gd | ( no_error="false"; while read -r line
+do
+    echo "$line"
+    if echo "$line" | grep -q "failing asserts:  0"; then
+        no_error="true"
+    fi
+done
+if [[ "$no_error" == "true" ]]; then
+    exit 0
+else
+    echo "ERROR: Some assertions failed !"
+    exit 1
+fi
+); exit $?
