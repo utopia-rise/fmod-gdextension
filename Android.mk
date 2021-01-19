59d81dc9cfe1ed83fb81d0f5ff91b88574e872ba
2shady4u <jd16581556@gmail.com>
2021-01-19 22:09:19 +0100

    Add Github Actions for all platforms

--- Android.mk
+++ Android.mk
@@ -3,24 +3,24 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := fmod-core-prebuilt
-LOCAL_SRC_FILES := ../libs/fmod/android/core/lib/$(TARGET_ARCH_ABI)/libfmod.so
+LOCAL_SRC_FILES := libs/fmod/android/core/lib/$(TARGET_ARCH_ABI)/libfmod.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := fmod-studio-prebuilt
-LOCAL_SRC_FILES := ../libs/fmod/android/studio/lib/$(TARGET_ARCH_ABI)/libfmodstudio.so
+LOCAL_SRC_FILES := libs/fmod/android/studio/lib/$(TARGET_ARCH_ABI)/libfmodstudio.so
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := godot-prebuilt
 ifeq ($(TARGET_ARCH_ABI),x86)
-    LOCAL_SRC_FILES := ../godot-cpp/bin/libgodot-cpp.android.release.x86.a
+    LOCAL_SRC_FILES := godot-cpp/bin/libgodot-cpp.android.release.x86.a
 endif
 ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
-    LOCAL_SRC_FILES := ../godot-cpp/bin/libgodot-cpp.android.release.armv7.a
+    LOCAL_SRC_FILES := godot-cpp/bin/libgodot-cpp.android.release.armv7.a
 endif
 ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
-    LOCAL_SRC_FILES := ../godot-cpp/bin/libgodot-cpp.android.release.arm64v8.a
+    LOCAL_SRC_FILES := godot-cpp/bin/libgodot-cpp.android.release.arm64v8.a
 endif
 include $(PREBUILT_STATIC_LIBRARY)
 
@@ -39,12 +39,12 @@ fmod-core-prebuilt \
 fmod-studio-prebuilt
 
 LOCAL_C_INCLUDES := \
-../godot-cpp/godot_headers \
-../godot-cpp/include/ \
-../godot-cpp/include/core \
-../godot-cpp/include/gen \
-../libs/fmod/android/studio/inc \
-../libs/fmod/android/core/inc
+godot-cpp/godot_headers \
+godot-cpp/include/ \
+godot-cpp/include/core \
+godot-cpp/include/gen \
+libs/fmod/android/studio/inc \
+libs/fmod/android/core/inc
 
 LOCAL_STATIC_LIBRARIES := godot-prebuilt
 
---
+++ get_fmod.py
@@ -0,0 +1,47 @@
+import requests
+import sys
+
+argv = sys.argv[1:] 
+
+user = argv[0]
+password = argv[1]
+platform = argv[2]
+
+fmodlink = "https://www.fmod.com/api-login"
+
+if platform == 'linux':
+    # linux
+    filename = 'fmodstudioapi20002linux.tar.gz'
+    downloadlink = 'https://www.fmod.com/api-get-download-link?path=files/fmodstudio/api/Linux/&filename=fmodstudioapi20002linux.tar.gz&user='
+elif platform == 'osx':
+    # OS X
+    filename = 'fmodstudioapi20002osx.dmg'
+    downloadlink = 'https://www.fmod.com/api-get-download-link?path=files/fmodstudio/api/Mac/&filename=fmodstudioapi20002mac-installer.dmg&user='
+elif platform == 'windows':
+    # Windows...
+    filename = 'fmodstudioapi20002win-installer.exe'
+    downloadlink = 'https://www.fmod.com/api-get-download-link?path=files/fmodstudio/api/Win/&filename=fmodstudioapi20002win-installer.exe&user='
+elif platform == 'android':
+    # Android...
+    filename = 'fmodstudioapi20002android.tar.gz'
+    downloadlink = 'https://www.fmod.com/api-get-download-link?path=files/fmodstudio/api/Android/&filename=fmodstudioapi20002android.tar.gz&user='
+elif platform == 'ios':
+    # iOS...
+    filename = 'fmodstudioapi20002ios.dmg'
+    downloadlink = 'https://www.fmod.com/api-get-download-link?path=files/fmodstudio/api/iOS/&filename=fmodstudioapi20002ios-installer.dmg&user=$1'
+
+downloadlink += user
+
+response = requests.post(fmodlink, auth = (user, password)).json()
+token = response["token"]
+
+print(token)
+
+response = requests.get(downloadlink, headers = {"Authorization": f"Bearer {token}"}).json()
+url = response["url"]
+
+response = requests.get(url, allow_redirects=True)
+open(filename, 'wb').write(response.content)
+
+print(downloadlink)
+print(response)
