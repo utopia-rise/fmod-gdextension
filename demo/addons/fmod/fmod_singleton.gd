5c71de3f74e626e979fdd807cef8ccc607c397b1
Tristan Grespinet <31470327+CedNaru@users.noreply.github.com>
2021-01-25 07:16:21 +0100

    Autoload is now a scene. (#65)

--- demo/addons/fmod/fmod_singleton.gd
+++ demo/addons/fmod/fmod_singleton.gd
@@ -2,7 +2,7 @@ tool
 extends EditorPlugin
 
 func _enter_tree():
-	add_autoload_singleton("Fmod", "res://addons/fmod/Fmod.gd")
+	add_autoload_singleton("Fmod", "res://addons/fmod/Fmod.tscn")
 
 func _exit_tree():
 	remove_autoload_singleton("Fmod")
