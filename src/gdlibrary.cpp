13213a40858f9c5654ed05abc7c54d7348710ea1
Tristan Grespinet <dev003@infra.utopia-rise.com>
2021-01-28 08:34:51 +0100

    fix file singleton release

--- src/gdlibrary.cpp
+++ src/gdlibrary.cpp
@@ -4,12 +4,11 @@ using namespace godot;
 
 extern "C" void GDN_EXPORT fmod_gdnative_init(godot_gdnative_init_options *o){
     Godot::gdnative_init(o);
-    //Just to initialize the singleton();
+    //Just to initialize the singleton;
     Callbacks::GodotFileRunner::get_singleton();
 }
 
 extern "C" void GDN_EXPORT fmod_gdnative_terminate(godot_gdnative_terminate_options *o){
-    delete Callbacks::GodotFileRunner::get_singleton();
     Godot::gdnative_terminate(o);
 }
 
