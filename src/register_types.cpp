c0c87ea2c0baf8c0d44cdf4d8537910c4133aa8e
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-07-27 20:37:49 +0100

    :construction: initial setup, includes and gdscript not adjusted yet

---
+++ src/register_types.cpp
@@ -0,0 +1,42 @@
+#include "register_types.h"
+
+#include <godot/gdnative_interface.h>
+
+#include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+#include "godot_fmod.h"
+
+using namespace godot;
+
+void initialize_fmod_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+
+    Callbacks::GodotFileRunner::get_singleton();
+
+    ClassDB::register_class<FMOD>();
+}
+
+void uninitialize_fmod_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
+
+extern "C" {
+
+// Initialization.
+
+GDNativeBool GDN_EXPORT fmod_library_init(const GDNativeInterface *p_interface, const GDNativeExtensionClassLibraryPtr p_library, GDNativeInitialization *r_initialization) {
+	godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+
+	init_obj.register_initializer(initialize_fmod_module);
+	init_obj.register_terminator(uninitialize_fmod_module);
+	init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+
+	return init_obj.init();
+}
+}
---
+++ src/register_types.h
@@ -0,0 +1,10 @@
+#ifndef FMOD_REGISTER_TYPES_H
+#define FMOD_REGISTER_TYPES_H
+
+#include <godot_cpp/core/class_db.hpp>
+using namespace godot;
+
+void initialize_example_module(ModuleInitializationLevel p_level);
+void uninitialize_example_module(ModuleInitializationLevel p_level);
+
+#endif // ! FMOD_REGISTER_TYPES_H
