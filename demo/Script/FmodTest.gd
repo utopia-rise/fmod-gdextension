daad9f3f7202dfc7960ce53949e04f48fcd8322b
Miguel Gonzalez Sanchez <822035+bitbrain@users.noreply.github.com>
2022-08-14 19:32:21 +0100

    fix further gdscript issues

--- demo/Script/FmodTest.gd
+++ demo/Script/FmodTest.gd
@@ -3,18 +3,18 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _enter_tree():
 	# set up FMOD
-	Fmod.set_software_format(0, Fmod.FMOD_SPEAKERMODE_STEREO, 0)
-	Fmod.init(1024, Fmod.FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.FMOD_INIT_NORMAL)
+	Fmod.set_software_format(0, Fmod.GD_FMOD_SPEAKERMODE_STEREO, 0)
+	Fmod.init(1024, Fmod.GD_FMOD_STUDIO_INIT_LIVEUPDATE, Fmod.GD_FMOD_INIT_NORMAL)
 	Fmod.set_sound_3D_settings(1, 32, 1)
 	Fmod.set_listener_number(2)
 	
 	# load banks
 # warning-ignore:return_value_discarded
-	Fmod.load_bank("res://assets/Banks/Master.strings.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://assets/Banks/Master.strings.bank", Fmod.GD_FMOD_STUDIO_LOAD_BANK_NORMAL)
 # warning-ignore:return_value_discarded
-	Fmod.load_bank("res://assets/Banks/Master.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://assets/Banks/Master.bank", Fmod.GD_FMOD_STUDIO_LOAD_BANK_NORMAL)
 # warning-ignore:return_value_discarded
-	Fmod.load_bank("res://assets/Banks/Music.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://assets/Banks/Music.bank", Fmod.GD_FMOD_STUDIO_LOAD_BANK_NORMAL)
 # warning-ignore:return_value_discarded
-	Fmod.load_bank("res://assets/Banks/Vehicles.bank", Fmod.FMOD_STUDIO_LOAD_BANK_NORMAL)
+	Fmod.load_bank("res://assets/Banks/Vehicles.bank", Fmod.GD_FMOD_STUDIO_LOAD_BANK_NORMAL)
 	print("Fmod initialised.")
