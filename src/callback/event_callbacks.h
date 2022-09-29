1190da7ce1a1ce7adb7c84721f8795b98dd08a1d
Tristan Grespinet <dev003@infra.utopia-rise.com>
2022-09-29 05:21:09 +0200

    add other files

--- src/callback/event_callbacks.h
+++ src/callback/event_callbacks.h
@@ -1,9 +1,9 @@
 #ifndef GODOTFMOD_GODOT_FMOD_CALLBACK_H
 #define GODOTFMOD_GODOT_FMOD_CALLBACK_H
 
+#include <core/Dictionary.hpp>
 #include <fmod_common.h>
 #include <fmod_studio_common.h>
-#include <core/Dictionary.hpp>
 #include <mutex>
 
 namespace Callbacks {
@@ -18,7 +18,7 @@ namespace Callbacks {
 
     extern std::mutex callback_mut;
 
-    FMOD_RESULT F_CALLBACK eventCallback(FMOD_STUDIO_EVENT_CALLBACK_TYPE type, FMOD_STUDIO_EVENTINSTANCE *event, void *parameters);
-}
+    FMOD_RESULT F_CALLBACK eventCallback(FMOD_STUDIO_EVENT_CALLBACK_TYPE type, FMOD_STUDIO_EVENTINSTANCE* event, void* parameters);
+}// namespace Callbacks
 
-#endif //GODOTFMOD_GODOT_FMOD_CALLBACK_H
+#endif// GODOTFMOD_GODOT_FMOD_CALLBACK_H
--- src/helpers/current_function.h
+++ src/helpers/current_function.h
@@ -4,7 +4,7 @@
 // MS compatible compilers support #pragma once
 
 #if defined(_MSC_VER) && (_MSC_VER >= 1020)
-# pragma once
+    #pragma once
 #endif
 
 //
@@ -19,49 +19,44 @@
 //  http://www.boost.org/libs/utility/current_function.html
 //
 
-namespace boost
-{
+namespace boost {
 
-    namespace detail
-    {
+    namespace detail {
 
-        inline void current_function_helper()
-        {
-
+        inline void current_function_helper() {
 #if defined(__GNUC__) || (defined(__MWERKS__) && (__MWERKS__ >= 0x3000)) || (defined(__ICC) && (__ICC >= 600))
 
-# define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
+    #define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
 
 #elif defined(__DMC__) && (__DMC__ >= 0x810)
 
-        # define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
+    #define BOOST_CURRENT_FUNCTION __PRETTY_FUNCTION__
 
 #elif defined(__FUNCSIG__)
 
-# define BOOST_CURRENT_FUNCTION __FUNCSIG__
+    #define BOOST_CURRENT_FUNCTION __FUNCSIG__
 
 #elif (defined(__INTEL_COMPILER) && (__INTEL_COMPILER >= 600)) || (defined(__IBMCPP__) && (__IBMCPP__ >= 500))
 
-# define BOOST_CURRENT_FUNCTION __FUNCTION__
+    #define BOOST_CURRENT_FUNCTION __FUNCTION__
 
 #elif defined(__BORLANDC__) && (__BORLANDC__ >= 0x550)
 
-# define BOOST_CURRENT_FUNCTION __FUNC__
+    #define BOOST_CURRENT_FUNCTION __FUNC__
 
 #elif defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 199901)
 
-# define BOOST_CURRENT_FUNCTION __func__
+    #define BOOST_CURRENT_FUNCTION __func__
 
 #else
 
-# define BOOST_CURRENT_FUNCTION "(unknown)"
+    #define BOOST_CURRENT_FUNCTION "(unknown)"
 
 #endif
-
         }
 
-    } // namespace detail
+    }// namespace detail
 
-} // namespace boost
+}// namespace boost
 
-#endif // #ifndef BOOST_CURRENT_FUNCTION_HPP_INCLUDED
+#endif// #ifndef BOOST_CURRENT_FUNCTION_HPP_INCLUDED
