59984f8555801c1d4d24f3e6836245f00475b8d3
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2023-08-24 18:20:15 +0200

    fix: Fmod event emitter now works as in 3.x and code has been simplified

--- src/nodes/fmod_event_emitter.h
+++ src/nodes/fmod_event_emitter.h
@@ -5,10 +5,12 @@
 
 #include <fmod_server.h>
 
+#include <classes/engine.hpp>
+
 namespace godot {
 
-    struct FmodEventEmitter {
-
+    template<class Derived, class NodeType>
+    class FmodEventEmitter : public NodeType {
         Ref<FmodEvent> _event;
 
         String _event_name;
@@ -17,59 +19,348 @@ namespace godot {
         bool _is_one_shot = false;
         bool _allow_fadeout = true;
         bool _preload_event = true;
+        float _volume = true;
 
         Dictionary _params;
+        
+    public:
+        virtual void _ready() override;
+        virtual void _process(double delta) override;
+        void _notification(int p_what);
+        virtual void _exit_tree() override;
+        
+        void start();
+        void stop();
+        void play();
+        void pause();
+        const Ref<FmodEvent>& get_event() const;
+        bool is_paused();
+        void set_event_name(const String& name);
+        String get_event_name() const;
+        void set_attached(const bool attached);
+        bool is_attached() const;
+        void set_autoplay(const bool autoplay);
+        bool is_autoplay() const;
+        void set_one_shot(const bool p_is_one_shot);
+        bool is_one_shot() const;
+        void set_allow_fadeout(const bool allow_fadeout);
+        bool is_allow_fadeout() const;
+        void set_preload_event(const bool preload_event);
+        bool is_preload_event() const;
+        void set_volume(const float volume);
+        float get_volume() const;
+        void set_parameter(const String& key, const float value);
+        const Dictionary& get_parameters() const;
+        void set_parameters(const Dictionary& p_params);
 
-        void set_param(const String& key, const float value) {
-            _params[key] = value;
-            if (!_event.is_valid()) { return; }
+        static void _bind_methods();
+        static StringName& get_class_static();
+
+    private:
+        void set_space_attribute() const;
+        void apply_parameters();
+        void preload_event() const;
+        void load_event();
+    };
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_space_attribute() const {
+        static_cast<const Derived*>(this)->set_space_attribute_impl();
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::_ready() {
+#ifdef TOOLS_ENABLED
+        // ensure we only run FMOD when the game is running!
+        if (Engine::get_singleton()->is_editor_hint()) { return; }
+#endif
+        
+        if (is_preload_event()) {
+            preload_event();
+        }
+
+        if (!_is_one_shot) {
+            load_event();
+            _event->set_volume(_volume);
+            apply_parameters();
+            set_space_attribute();
+        }
+
+        if (_autoplay) { play(); }
+    }
+    
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::_process(double delta) {
+#ifdef TOOLS_ENABLED
+        if (Engine::get_singleton()->is_editor_hint()) { return; }
+#endif
+
+        bool should_restart{ false };
+        if (!_event.is_valid() && !_is_one_shot && _autoplay) {
+            load_event();
+            _event->set_volume(_volume);
+            apply_parameters();
+            should_restart = true;
+        }
+
+        if (!_is_one_shot && _attached && _event.is_valid()) {
+            set_space_attribute();
+        }
+
+        if (should_restart) {
+            play();
+        }
+    }
+    
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::_notification(int p_what) {
+#ifdef TOOLS_ENABLED
+        // ensure we only run FMOD when the game is running!
+        if (Engine::get_singleton()->is_editor_hint()) { return; }
+#endif
+
+        if (p_what == Node::NOTIFICATION_PAUSED) {
+            pause();
+        } else if (p_what == Node::NOTIFICATION_UNPAUSED) {
+            if (is_paused()) { play(); }
+        }
+    }
+    
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::_exit_tree() {
+#ifdef TOOLS_ENABLED
+        if (Engine::get_singleton()->is_editor_hint()) { return; }
+#endif
+
+        stop();
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_parameter(const String& key, const float value) {
+        _params[key] = value;
+        if (!_event.is_valid()) { return; }
+        _event->set_parameter_by_name(key, _params[key]);
+    }
+
+    template<class Derived, class NodeType>
+    const Dictionary& FmodEventEmitter<Derived, NodeType>::get_parameters() const {
+        return _params;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_parameters(const Dictionary& p_params) {
+        _params = p_params;
+        apply_parameters();
+    }
+
+    template<class Derived, class NodeType>
+    bool FmodEventEmitter<Derived, NodeType>::is_paused() {
+        if (!_event.is_valid()) { return false; }
+        return _event->get_paused();
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::start() {
+        if (!_event.is_valid()) { return; }
+        _event->start();
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::stop() {
+        if (!_event.is_valid()) { return; }
+        if (_allow_fadeout) {
+            _event->stop(FMOD_STUDIO_STOP_ALLOWFADEOUT);
+        } else {
+            _event->stop(FMOD_STUDIO_STOP_IMMEDIATE);
+        }
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::play() {
+        if (!_event.is_valid()) { return; }
+        _event->set_paused(false);
+
+        if (!_is_one_shot) {
+            if (_attached) { set_space_attribute(); }
+            start();
+            return;
+        }
+
+        if (_attached) {
+            if (!_params.is_empty()) {
+                FmodServer::get_singleton()->play_one_shot_attached_with_params(
+                  _event_name,
+                  this,
+                  _params
+                );
+                return;
+            }
+
+            FmodServer::get_singleton()->play_one_shot_attached(_event_name, this);
+            return;
+        }
+
+        if (!_params.is_empty()) {
+            FmodServer::get_singleton()->play_one_shot_with_params(
+              _event_name,
+              this,
+              _params
+            );
+            return;
+        }
+
+        FmodServer::get_singleton()->play_one_shot(_event_name, this);
+        return;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::pause() {
+        if (!_event.is_valid()) { return; }
+        _event->set_paused(true);
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::preload_event() const {
+        Ref<FmodEventDescription> desc = FmodServer::get_singleton()->get_event(_event_name);
+        desc->load_sample_data();
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::load_event() {
+        _event = FmodServer::get_singleton()->create_event_instance(_event_name);
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::apply_parameters() {
+        if (!_event.is_valid()) { return; }
+        for (int i = 0; i < _params.keys().size(); ++i) {
+            const String& key = _params.keys()[i];
             _event->set_parameter_by_name(key, _params[key]);
         }
+    }
 
-        bool is_paused() {
-            if (!_event.is_valid()) { return false; }
-            return _event->get_paused();
+    template<class Derived, class NodeType>
+    const Ref<FmodEvent>& FmodEventEmitter<Derived, NodeType>::get_event() const {
+        return _event;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_event_name(const String& name) {
+        _event_name = name;
+    }
+
+    template<class Derived, class NodeType>
+    String FmodEventEmitter<Derived, NodeType>::get_event_name() const {
+        return _event_name;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_attached(const bool attached) {
+        _attached = attached;
+    }
+
+    template<class Derived, class NodeType>
+    bool FmodEventEmitter<Derived, NodeType>::is_attached() const {
+        return _attached;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_autoplay(const bool autoplay) {
+        _autoplay = autoplay;
+    }
+
+    template<class Derived, class NodeType>
+    bool FmodEventEmitter<Derived, NodeType>::is_autoplay() const {
+        return _autoplay;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_one_shot(const bool p_is_one_shot) {
+        _is_one_shot = p_is_one_shot;
+    }
+
+    template<class Derived, class NodeType>
+    bool FmodEventEmitter<Derived, NodeType>::is_one_shot() const {
+        return _is_one_shot;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_allow_fadeout(const bool allow_fadeout) {
+        _allow_fadeout = allow_fadeout;
+    }
+
+    template<class Derived, class NodeType>
+    bool FmodEventEmitter<Derived, NodeType>::is_allow_fadeout() const {
+        return _allow_fadeout;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_preload_event(const bool preload_event) {
+        _preload_event = preload_event;
+    }
+
+    template<class Derived, class NodeType>
+    bool FmodEventEmitter<Derived, NodeType>::is_preload_event() const {
+        return _preload_event;
+    }
+
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::set_volume(const float volume) {
+        _volume = volume;
+
+        if (!_event.is_valid()) {
+            return;
         }
+        _event->set_volume(volume);
+    }
 
-        void play() {
-            if (!_event.is_valid()) { return; }
-            _event->set_paused(false);
-        }
+    template<class Derived, class NodeType>
+    float FmodEventEmitter<Derived, NodeType>::get_volume() const {
+        return _volume;
+    }
 
-        void pause() {
-            if (!_event.is_valid()) { return; }
-            _event->set_paused(true);
-        }
+    template<class Derived, class NodeType>
+    StringName& FmodEventEmitter<Derived, NodeType>::get_class_static() {
+        return Derived::get_class_static();
+    }
 
-        void set_event_name(const String& name) {
-            if (name.begins_with("event:/")) {
-                _event_name = name;
-            } else {
-                _event_name = "event:/" + name;
-            }
-        }
+    template<class Derived, class NodeType>
+    void FmodEventEmitter<Derived, NodeType>::_bind_methods() {
+        ClassDB::bind_method(D_METHOD("set_parameter", "key", "value"), &Derived::set_parameter);
+        ClassDB::bind_method(D_METHOD("is_paused"), &Derived::is_paused);
+        ClassDB::bind_method(D_METHOD("play"), &Derived::play);
+        ClassDB::bind_method(D_METHOD("pause"), &Derived::pause);
+        ClassDB::bind_method(D_METHOD("set_event_name", "event_name"), &Derived::set_event_name);
+        ClassDB::bind_method(D_METHOD("get_event_name"), &Derived::get_event_name);
+        ClassDB::bind_method(D_METHOD("set_attached", "attached"), &Derived::set_attached);
+        ClassDB::bind_method(D_METHOD("is_attached"), &Derived::is_attached);
+        ClassDB::bind_method(D_METHOD("set_autoplay", "_autoplay"), &Derived::set_autoplay);
+        ClassDB::bind_method(D_METHOD("is_autoplay"), &Derived::is_autoplay);
+        ClassDB::bind_method(D_METHOD("set_one_shot", "_is_one_shot"), &Derived::set_one_shot);
+        ClassDB::bind_method(D_METHOD("is_one_shot"), &Derived::is_one_shot);
+        ClassDB::bind_method(D_METHOD("set_allow_fadeout", "allow_fadeout"), &Derived::set_allow_fadeout);
+        ClassDB::bind_method(D_METHOD("is_allow_fadeout"), &Derived::is_allow_fadeout);
+        ClassDB::bind_method(D_METHOD("set_preload_event", "preload_event"), &Derived::set_preload_event);
+        ClassDB::bind_method(D_METHOD("is_preload_event"), &Derived::is_preload_event);
+        ClassDB::bind_method(D_METHOD("_notification", "p_what"), &Derived::_notification);
+        ClassDB::bind_method(D_METHOD("get_parameters"), &Derived::get_parameters);
+        ClassDB::bind_method(D_METHOD("set_parameters", "p_parameters"), &Derived::set_parameters);
+        ClassDB::bind_method(D_METHOD("get_volume"), &Derived::get_volume);
+        ClassDB::bind_method(D_METHOD("set_volume", "p_volume"), &Derived::set_volume);
 
-        String get_event_name() { return _event_name; }
+        ADD_PROPERTY(PropertyInfo(Variant::STRING, "event_name",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_event_name", "get_event_name");
+        ADD_PROPERTY(PropertyInfo(Variant::BOOL, "attached",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_attached", "is_attached");
+        ADD_PROPERTY(PropertyInfo(Variant::BOOL, "autoplay",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_autoplay", "is_autoplay");
+        ADD_PROPERTY(PropertyInfo(Variant::BOOL, "one_shot",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_one_shot", "is_one_shot");
+        ADD_PROPERTY(PropertyInfo(Variant::BOOL, "allow_fadeout",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_allow_fadeout", "is_allow_fadeout");
+        ADD_PROPERTY(PropertyInfo(Variant::BOOL, "preload_event",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_preload_event", "is_preload_event");
+        ADD_PROPERTY(PropertyInfo(Variant::DICTIONARY, "parameters",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_parameters", "get_parameters");
+        ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "volume",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT), "set_volume", "get_volume");
 
-        void set_attached(const bool attached) { this->_attached = attached; }
-
-        bool is_attached() const { return _attached; }
-
-        void set_autoplay(const bool autoplay) { this->_autoplay = autoplay; }
-
-        bool is_autoplay() const { return _autoplay; }
-
-        void set_looped(const bool looped) { this->_is_one_shot = looped; }
-
-        bool is_looped() const { return _is_one_shot; }
-
-        void set_allow_fadeout(const bool allow_fadeout) { this->_allow_fadeout = allow_fadeout; }
-
-        bool is_allow_fadeout() const { return _allow_fadeout; }
-
-        void set_preload_event(const bool preload_event) { this->_preload_event = preload_event; }
-
-        bool is_preload_event() const { return _preload_event; }
-    };
+        ADD_SIGNAL(MethodInfo("timeline_beat", PropertyInfo(Variant::DICTIONARY, "params")));
+        ADD_SIGNAL(MethodInfo("timeline_marker", PropertyInfo(Variant::DICTIONARY, "params")));
+        ADD_SIGNAL(MethodInfo("sound_played", PropertyInfo(Variant::DICTIONARY, "params")));
+        ADD_SIGNAL(MethodInfo("sound_stopped", PropertyInfo(Variant::DICTIONARY, "params")));
+    }
 }// namespace godot
 #endif// GODOTFMOD_FMOD_EVENT_EMITTER_H
--- src/nodes/fmod_event_emitter_2d.cpp
+++ src/nodes/fmod_event_emitter_2d.cpp
@@ -1,127 +1,27 @@
 #include <nodes/fmod_event_emitter_2d.h>
 
-#include <classes/engine.hpp>
-
 using namespace godot;
 
-void FmodEventEmitter2D::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("set_param", "key", "value"), &FmodEventEmitter2D::set_param);
-    ClassDB::bind_method(D_METHOD("is_paused"), &FmodEventEmitter2D::is_paused);
-    ClassDB::bind_method(D_METHOD("play"), &FmodEventEmitter2D::play);
-    ClassDB::bind_method(D_METHOD("pause"), &FmodEventEmitter2D::pause);
-    ClassDB::bind_method(D_METHOD("set_event_name", "event_name"), &FmodEventEmitter2D::set_event_name);
-    ClassDB::bind_method(D_METHOD("get_event_name"), &FmodEventEmitter2D::get_event_name);
-    ClassDB::bind_method(D_METHOD("set_attached", "attached"), &FmodEventEmitter2D::set_attached);
-    ClassDB::bind_method(D_METHOD("is_attached"), &FmodEventEmitter2D::is_attached);
-    ClassDB::bind_method(D_METHOD("set_autoplay", "_autoplay"), &FmodEventEmitter2D::set_autoplay);
-    ClassDB::bind_method(D_METHOD("is_autoplay"), &FmodEventEmitter2D::is_autoplay);
-    ClassDB::bind_method(D_METHOD("set_looped", "_is_one_shot"), &FmodEventEmitter2D::set_looped);
-    ClassDB::bind_method(D_METHOD("is_looped"), &FmodEventEmitter2D::is_looped);
-    ClassDB::bind_method(D_METHOD("set_allow_fadeout", "allow_fadeout"), &FmodEventEmitter2D::set_allow_fadeout);
-    ClassDB::bind_method(D_METHOD("is_allow_fadeout"), &FmodEventEmitter2D::is_allow_fadeout);
-    ClassDB::bind_method(D_METHOD("set_preload_event", "preload_event"), &FmodEventEmitter2D::set_preload_event);
-    ClassDB::bind_method(D_METHOD("is_preload_event"), &FmodEventEmitter2D::is_preload_event);
-    ClassDB::bind_method(D_METHOD("_notification", "p_what"), &FmodEventEmitter2D::_notification);
-
-    ADD_PROPERTY(PropertyInfo(Variant::STRING, "event_name",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE), "set_event_name", "get_event_name");
-    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "attached",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE), "set_attached", "is_attached");
-    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "autoplay",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE), "set_autoplay", "is_autoplay");
-    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "looped",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE), "set_looped", "is_looped");
-    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "allow_fadeout",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE), "set_allow_fadeout", "is_allow_fadeout");
-    ADD_PROPERTY(PropertyInfo(Variant::BOOL, "preload_event",PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NONE), "set_preload_event", "is_preload_event");
-
-    ADD_SIGNAL(MethodInfo("timeline_beat", PropertyInfo(Variant::DICTIONARY, "params")));
-    ADD_SIGNAL(MethodInfo("timeline_marker", PropertyInfo(Variant::DICTIONARY, "params")));
-    ADD_SIGNAL(MethodInfo("sound_played", PropertyInfo(Variant::DICTIONARY, "params")));
-    ADD_SIGNAL(MethodInfo("sound_stopped", PropertyInfo(Variant::DICTIONARY, "params")));
+void FmodEventEmitter2D::set_space_attribute_impl() const {
+    get_event()->set_2d_attributes(get_global_transform());
 }
 
 void FmodEventEmitter2D::_ready() {
-    // ensure we only run FMOD when the game is running!
-    if (Engine::get_singleton()->is_editor_hint()) { return; }
-
-    if (internal_emitter._preload_event) {
-        Ref<FmodEventDescription> desc = FmodServer::get_singleton()->get_event(internal_emitter._event_name);
-        desc->load_sample_data();
-    }
-
-    internal_emitter._event = FmodServer::get_singleton()->create_event_instance(internal_emitter._event_name);
-
-    for (int i = 0; i < internal_emitter._params.keys().size(); ++i) {
-        auto key = internal_emitter._params.keys()[i];
-        internal_emitter._event->set_parameter_by_name(key, internal_emitter._params[key]);
-    }
-
-    internal_emitter._event->set_2d_attributes(get_transform());
-    if (internal_emitter._autoplay) { play(); }
+    FmodEventEmitter<FmodEventEmitter2D, Node2D>::_ready();
 }
 
 void FmodEventEmitter2D::_process(double delta) {
-    if (!internal_emitter._event.is_valid() && !internal_emitter._is_one_shot && internal_emitter._autoplay) {
-        internal_emitter._event = FmodServer::get_singleton()->create_event_instance(internal_emitter._event_name);
-    }
-
-    if (internal_emitter._attached && internal_emitter._event.is_valid()) { internal_emitter._event->set_2d_attributes(get_transform()); }
+    FmodEventEmitter<FmodEventEmitter2D, Node2D>::_process(delta);
 }
 
 void FmodEventEmitter2D::_notification(int p_what) {
-    // ensure we only run FMOD when the game is running!
-    if (Engine::get_singleton()->is_editor_hint()) { return; }
-    if (p_what == NOTIFICATION_PAUSED) {
-        pause();
-    } else if (p_what == NOTIFICATION_UNPAUSED) {
-        if (is_paused()) { play(); }
-    }
+    FmodEventEmitter<FmodEventEmitter2D, Node2D>::_notification(p_what);
 }
 
 void FmodEventEmitter2D::_exit_tree() {
-    if (!internal_emitter._event.is_valid()) { return; }
-
-    if (internal_emitter._allow_fadeout) {
-        internal_emitter._event->stop(FMOD_STUDIO_STOP_ALLOWFADEOUT);
-    } else {
-        internal_emitter._event->stop(FMOD_STUDIO_STOP_IMMEDIATE);
-    }
+    FmodEventEmitter<FmodEventEmitter2D, Node2D>::_exit_tree();
 }
 
-void FmodEventEmitter2D::set_param(const String& key, const float value) {
-    internal_emitter.set_param(key, value);
+void FmodEventEmitter2D::_bind_methods() {
+    FmodEventEmitter<FmodEventEmitter2D, Node2D>::_bind_methods();
 }
-
-bool FmodEventEmitter2D::is_paused() {
-    return internal_emitter.is_paused();
-}
-
-void FmodEventEmitter2D::play() {
-    internal_emitter.play();
-}
-
-void FmodEventEmitter2D::pause() {
-    internal_emitter.pause();
-}
-
-void FmodEventEmitter2D::set_event_name(const String& name) {
-    internal_emitter.set_event_name(name);
-}
-
-String FmodEventEmitter2D::get_event_name() { return internal_emitter._event_name; }
-
-void FmodEventEmitter2D::set_attached(const bool attached) { internal_emitter._attached = attached; }
-
-bool FmodEventEmitter2D::is_attached() const { return internal_emitter._attached; }
-
-void FmodEventEmitter2D::set_autoplay(const bool autoplay) { internal_emitter._autoplay = autoplay; }
-
-bool FmodEventEmitter2D::is_autoplay() const { return internal_emitter._autoplay; }
-
-void FmodEventEmitter2D::set_looped(const bool looped) { internal_emitter._is_one_shot = !looped; }
-
-bool FmodEventEmitter2D::is_looped() const { return !internal_emitter._is_one_shot; }
-
-void FmodEventEmitter2D::set_allow_fadeout(const bool allow_fadeout) { internal_emitter._allow_fadeout = allow_fadeout; }
-
-bool FmodEventEmitter2D::is_allow_fadeout() const { return internal_emitter._allow_fadeout; }
-
-void FmodEventEmitter2D::set_preload_event(const bool preload_event) { internal_emitter._preload_event = preload_event; }
-
-bool FmodEventEmitter2D::is_preload_event() const { return internal_emitter._preload_event; }
