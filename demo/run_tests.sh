cb48f0344c100073e96050b7a90960e2662e6505
bitbrain <miguel-gonzalez@gmx.de>
2023-12-06 18:18:16 +0000

    fix script

--- demo/run_tests.sh
+++ demo/run_tests.sh
@@ -1,18 +1,36 @@
-$1 -s --headless --path $PWD addons/gut/gut_cmdln.gd | ( no_error="false"; while read -r line
-do
-    echo "$line"
-    if echo "$line" | grep -q "Passing tests     0"; then
-        echo "ERROR: No tests passed !"
+$1 -s --headless --path $PWD addons/gut/gut_cmdln.gd | (
+    tests=0
+    passing=0
+
+    while read -r line
+    do
+        echo "$line"
+
+        # Capture the number of tests
+        if echo "$line" | grep -q "Tests"; then
+            tests=$(echo "$line" | awk '{print $2}')
+        fi
+
+        # Capture the number of passing tests
+        if echo "$line" | grep -q "Passing"; then
+            passing=$(echo "$line" | awk '{print $2}')
+        fi
+    done
+
+    if [[ "$tests" -eq 0 ]]; then
+       echo "ERROR: No tests were found."
+       exit 1
+    fi
+
+    if [[ "$tests" -eq "$passing" ]]; then
+        exit 0
+    else
+        echo "ERROR: Some assertions failed!"
         exit 1
     fi
-    if echo "$line" | grep -q "Failing tests     0"; then
-        no_error="true"
-    fi
-done
-if [[ "$no_error" == "true" ]]; then
-    exit 0
-else
-    echo "ERROR: Some assertions failed !"
-    exit 1
-fi
-); exit $?
+)
+
+result=$?
+
+# Exit with the result of the subshell
+exit $result
