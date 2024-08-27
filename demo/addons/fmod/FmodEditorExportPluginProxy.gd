4d7ce607b7fd6a9730f664bfebfbee60c28b7375
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2024-08-27 14:50:06 +0200

    chore: remove FmodEditorExportPluginProxy

--- demo/addons/fmod/FmodEditorExportPluginProxy.gd
+++
@@ -1,5 +0,0 @@
-# Here because gdextension does not support editor export plugin for now.
-# https://github.com/godotengine/godot/issues/80593
-@tool
-class_name FmodEditorExportPluginProxy
-extends FmodEditorExportPlugin
