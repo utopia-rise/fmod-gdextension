fdb1d5937b473227e9bc54af44c0347bfa4c5edd
Pierre-Thomas Meisels <meisels27@yahoo.fr>
2024-12-04 19:42:40 +0100

    fix: Remove performance monitor on exit to avoid crash

--- demo/addons/fmod/FmodManager.gd
+++ demo/addons/fmod/FmodManager.gd
@@ -1,9 +1,16 @@
 @tool
 extends Node
 
+var performance_display: PerformancesDisplay
+
 func _ready():
 	process_mode = PROCESS_MODE_ALWAYS
-	add_child(PerformancesDisplay.new())
+	performance_display = PerformancesDisplay.new()
+	add_child(performance_display)
+
+func _exit_tree() -> void:
+	remove_child(performance_display)
+	performance_display.free()
 
 func _process(delta):
 	FmodServer.update()
