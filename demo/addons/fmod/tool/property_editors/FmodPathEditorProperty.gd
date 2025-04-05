17b5ddf5f41553fea4359d5ade5f30cf379579e1
Tristan Grespinet <cednaru.dev@gmail.com>
2025-04-05 20:44:43 +0200

    Update version to 5.0.5 and Godot 4.4 (#326)

--- demo/addons/fmod/tool/property_editors/FmodPathEditorProperty.gd
+++ demo/addons/fmod/tool/property_editors/FmodPathEditorProperty.gd
@@ -8,33 +8,33 @@ var regex := RegEx.new()
 var default_line_edit_modulate: Color
 
 func initialize(open_project_explorer_callable: Callable, path_prop: String, guid_prop: String):
-	regex.compile("{\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}}")
-	guid_property = guid_prop
-	path_property = path_prop
-	var guid_and_path_ui: FmodGuidAndPathPropertyEditorUi = %FmodGuidAndPathPropertyEditorUi
-	
-	default_line_edit_modulate = guid_and_path_ui.get_node("%GuidLineEdit").modulate
-	
-	var open_project_explorer_event = func open_project_explorer_event():
-		open_project_explorer_callable.call(self._set_path_and_guid)
-	guid_and_path_ui.set_callables(open_project_explorer_event, _set_path, _set_guid)
+    regex.compile("{\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}}")
+    guid_property = guid_prop
+    path_property = path_prop
+    var guid_and_path_ui: FmodGuidAndPathPropertyEditorUi = %FmodGuidAndPathPropertyEditorUi
+    
+    default_line_edit_modulate = guid_and_path_ui.get_node("%GuidLineEdit").modulate
+    
+    var open_project_explorer_event = func open_project_explorer_event():
+        open_project_explorer_callable.call(self._set_path_and_guid)
+    guid_and_path_ui.set_callables(open_project_explorer_event, _set_path, _set_guid)
 
 func _update_property():
-	var guid_and_path_ui = %FmodGuidAndPathPropertyEditorUi
-	guid_and_path_ui.get_node("%PathLineEdit").text = get_edited_object()[path_property]
-	guid_and_path_ui.get_node("%GuidLineEdit").text = get_edited_object()[guid_property]
+    var guid_and_path_ui = %FmodGuidAndPathPropertyEditorUi
+    guid_and_path_ui.get_node("%PathLineEdit").text = get_edited_object()[path_property]
+    guid_and_path_ui.get_node("%GuidLineEdit").text = get_edited_object()[guid_property]
 
 func _set_path(path: String):
-	emit_changed(path_property, path)
+    emit_changed(path_property, path)
 
 func _set_guid(guid: String):
-	var line_edit := %FmodGuidAndPathPropertyEditorUi.get_node("%GuidLineEdit") as LineEdit
-	if not regex.search(guid):
-		line_edit.modulate = Color.RED
-		return
-	line_edit.modulate = default_line_edit_modulate
-	emit_changed(guid_property, guid)
+    var line_edit := %FmodGuidAndPathPropertyEditorUi.get_node("%GuidLineEdit") as LineEdit
+    if not regex.search(guid):
+        line_edit.modulate = Color.RED
+        return
+    line_edit.modulate = default_line_edit_modulate
+    emit_changed(guid_property, guid)
 
 func _set_path_and_guid(path: String, guid: String):
-	_set_path(path)
-	_set_guid(guid)
+    _set_path(path)
+    _set_guid(guid)
