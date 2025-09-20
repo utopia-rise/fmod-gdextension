e978fee03f3b8a75ef24620bca9d31f979d9165e
Tristan Grespinet <cednaru.dev@gmail.com>
2025-09-20 03:35:50 +0200

    Run external PR on different environment. (#377)

--- .travis/download-godot-linux-headless.sh
+++
@@ -1,8 +0,0 @@
-GODOT_VERSION=3.1.1
-GODOT_BIN=Godot_v${GODOT_VERSION}-stable_linux_headless.64
-GODOT_ZIP=${GODOT_BIN}.zip
-GODOT_URL=https://downloads.tuxfamily.org/godotengine/${GODOT_VERSION}/${GODOT_ZIP}
-
-curl ${GODOT_URL} --output ${GODOT_ZIP}
-7z x ${GODOT_ZIP}
-chmod +x ${GODOT_BIN}
